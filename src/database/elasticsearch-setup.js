65f8489ef27f8d08370a23c7de17220e59fae5cb
myko <awesomeme155@gmail.com>
2020-03-07 13:23:47 +0300

    chore: deploy-to-heroku
    - Add Procfile
    - Configure elasticsearch add-on engine on heroku
    - Add heroku elasticsearch engine config to the app
    
    [Finishes #171601145]

--- src/database/elasticsearch-setup.js
+++ src/database/elasticsearch-setup.js
@@ -1,7 +1,7 @@
 import elasticsearch from 'elasticsearch';
 
 const db = new elasticsearch.Client({
-  host: `${process.env.ELASTICSEARCH_PROTOCOL}://${process.env.ELASTICSEARCH_HOSTNAME}:${process.env.ELASTICSEARCH_PORT}`,
+  host: process.env.BONSAI_URL || `${process.env.ELASTICSEARCH_PROTOCOL}://${process.env.ELASTICSEARCH_HOSTNAME}:${process.env.ELASTICSEARCH_PORT}`,
 });
 
 export default db;
